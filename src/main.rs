7ddadb9437f4ef58bd4e562841598c1b6d19775b
EiEddie <eieddie4u@outlook.com>
2023-12-09 20:47:05 +0800

    feat: 可移动元素

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use clap::Parser;
-use std::fmt;
+use std::{
+    fmt,
+    ops::{Add, AddAssign},
+};
 
 #[derive(Parser)]
 #[command(
@@ -23,12 +26,28 @@ struct Cli {
     is_story: bool,
 }
 
+#[derive(Clone, Copy)]
 struct Pos(i32, i32);
+
+impl Add for Pos {
+    type Output = Pos;
+    fn add(self, rhs: Self) -> Self::Output {
+        return Pos(self.0 + rhs.0, self.1 + rhs.1);
+    }
+}
+
+impl AddAssign for Pos {
+    fn add_assign(&mut self, rhs: Self) {
+        self.0 += rhs.0;
+        self.1 += rhs.1;
+    }
+}
 
 struct Field {
     width: usize,
     height: usize,
     val: Vec<i32>,
+    move_pnt: Pos,
 }
 
 impl Field {
@@ -37,17 +56,46 @@ impl Field {
             width,
             height,
             val: vec![0; width * height],
+            move_pnt: Pos(width as i32 / 2, height as i32 / 2),
         };
     }
 
+    /// 将索引转为 `field` 中的位置
     fn index_to_pos(&self, index: usize) -> Pos {
         return Pos((index % self.width) as i32, (index / self.width) as i32);
     }
 
+    /// 将 `field` 中的位置转为索引
     fn pos_to_index(&self, pos: Pos) -> usize {
         let w = self.width as i32;
         let h = self.height as i32;
         return ((pos.0 + pos.1 * w) % (w * h)) as usize;
+    }
+
+    /// 根据传入的方向移动两次
+    /// 仅 [`u8`] 的后 4 bit 为有效方向
+    /// - `00` LU
+    /// - `01` RU
+    /// - `10` LD
+    /// - `11` RD
+    fn pnt_move(&mut self, dirs: u8) -> Pos {
+        self.move_pnt += match dirs & 0b0011 {
+            0b0000 => Pos(-1, -1),
+            0b0001 => Pos(1, -1),
+            0b0010 => Pos(-1, 1),
+            0b0011 => Pos(1, 1),
+            _ => Pos(0, 0),
+        };
+
+        self.move_pnt += match dirs & 0b1100 {
+            0b0000 => Pos(-1, -1),
+            0b0100 => Pos(1, -1),
+            0b1000 => Pos(-1, 1),
+            0b1100 => Pos(1, 1),
+            _ => Pos(0, 0),
+        };
+
+        return self.move_pnt;
     }
 }
 
