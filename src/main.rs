1d76d6149a0a3918278f362bd73713aebd3ca8bf
EiEddie <eieddie4u@outlook.com>
2023-12-10 22:56:57 +0800

    chore: 格式化设置

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,54 @@
 use clap::{CommandFactory, Parser};
-use dbishop as db;
-use hex;
+use {dbishop as db, hex};
 
 mod error;
 
 #[derive(Parser, Debug)]
-#[command(
-    version,
-    about = "The hash fingerprint visualization algorithm, like OpenSSH"
-)]
+#[command(version,
+          about = "The hash fingerprint visualization algorithm, like OpenSSH")]
 struct Cli {
-    /// Input data, like a hex string
-    data: Option<String>,
+	/// Input data, like a hex string
+	data: Option<String>,
 
-    /// Don't echo hex input
-    #[arg(short = 'q', long = "quiet", action = clap::ArgAction::SetTrue)]
-    is_quiet: bool,
+	/// Don't echo hex input
+	#[arg(short = 'q', long = "quiet", action = clap::ArgAction::SetTrue)]
+	is_quiet: bool,
 
-    /// Use file, if '-' use stdin
-    #[arg(short = 'i', long = "in")]
-    file: Option<String>,
+	/// Use file, if '-' use stdin
+	#[arg(short = 'i', long = "in")]
+	file: Option<String>,
 
-    /// Read the story of Bishop Peter
-    #[arg(long = "story", action = clap::ArgAction::SetTrue)]
-    is_story: bool,
+	/// Read the story of Bishop Peter
+	#[arg(long = "story", action = clap::ArgAction::SetTrue)]
+	is_story: bool,
 }
 
 fn cli() -> Result<(), error::DBishopError> {
-    let cli = Cli::parse();
+	let cli = Cli::parse();
 
-    #[cfg(debug_assertions)]
-    dbg!(&cli);
+	#[cfg(debug_assertions)]
+	dbg!(&cli);
 
-    if cli.is_story {
-        todo!();
-    }
+	if cli.is_story {
+		todo!();
+	}
 
-    if cli.data == None && cli.file == None {
-        return Err(error::DBishopError::EmptyInput);
-    }
+	if cli.data == None && cli.file == None {
+		return Err(error::DBishopError::EmptyInput);
+	}
 
-    let seq = hex::decode(cli.data.as_ref().unwrap())?;
-    if !cli.is_quiet {
-        println!("fingerprint of `{}`:", cli.data.as_ref().unwrap());
-    }
-    print!("{}", db::fingerprint(seq));
+	let seq = hex::decode(cli.data.as_ref().unwrap())?;
+	if !cli.is_quiet {
+		println!("fingerprint of `{}`:", cli.data.as_ref().unwrap());
+	}
+	print!("{}", db::fingerprint(seq));
 
-    return Ok(());
+	return Ok(());
 }
 
 fn main() {
-    let mut cmd = Cli::command();
-    if let Err(e) = cli() {
-        cmd.error(e.into(), e.to_string()).exit();
-    }
+	let mut cmd = Cli::command();
+	if let Err(e) = cli() {
+		cmd.error(e.into(), e.to_string()).exit();
+	}
 }
