91d6bd1ca160f5600f1325c2ccece035ed403f57
EiEddie <eieddie4u@outlook.com>
2023-12-10 16:03:24 +0800

    feat: 错误类型

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,55 @@
-use clap::Parser;
+use clap::{error::ErrorKind, Parser};
+use core::fmt;
+use hex;
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum DBishopError {
+    /// 输入为空
+    EmptyInput,
+
+    /// 输入的十六进制字符串始终应为偶数
+    OddHexInputLength,
+
+    /// 输入的十六进制字符串存在非法字符
+    WrongHexInputChar { c: char, i: usize },
+}
+
+#[cfg(feature = "std")]
+impl std::error::Error for DBishopError {}
+
+impl fmt::Display for DBishopError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        return match *self {
+            Self::EmptyInput => write!(f, "Input is empty."),
+            Self::OddHexInputLength => write!(f, "The number of digits is not even."),
+            Self::WrongHexInputChar { c, i } => {
+                write!(f, "Input string has a worng char `{}` in {}.", c, i)
+            }
+        };
+    }
+}
+
+impl From<hex::FromHexError> for DBishopError {
+    fn from(value: hex::FromHexError) -> Self {
+        return match value {
+            hex::FromHexError::OddLength => DBishopError::OddHexInputLength,
+            hex::FromHexError::InvalidHexCharacter { c, index } => {
+                DBishopError::WrongHexInputChar { c, i: index }
+            }
+            hex::FromHexError::InvalidStringLength => todo!(),
+        };
+    }
+}
+
+impl Into<ErrorKind> for DBishopError {
+    fn into(self) -> ErrorKind {
+        return match self {
+            DBishopError::EmptyInput => ErrorKind::MissingRequiredArgument,
+            DBishopError::OddHexInputLength => ErrorKind::InvalidValue,
+            DBishopError::WrongHexInputChar { .. } => ErrorKind::InvalidValue,
+        };
+    }
+}
 
 #[derive(Parser)]
 #[command(
